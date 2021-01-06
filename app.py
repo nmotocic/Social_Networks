410bb6fdb320681e8df13b1e18436d5243e77d45
Matej Đanić <matej.danic@gmail.com>
2021-01-06 17:45:18 +0100

    implement recommender functionality and view
    recommender implemented using collaborative filtering based on similar users

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , redirect , url_for, session, request
+from flask import Flask, render_template, redirect, url_for, session, request
 from flask_dance.contrib.twitter import make_twitter_blueprint, twitter
 from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 from flask_dance.consumer import oauth_authorized
@@ -7,204 +7,261 @@ from social_network import dbComms
 from social_network import dbTestInfo
 from social_network import movieApiController
 from social_network import warnings
-from social_network.dbModels import User,Movie,Genre
+from social_network.dbModels import User, Movie, Genre
 from flask_scss import Scss
 import os
 import requests
 import json
+import sys
+from social_network.recommendations import get_recommendations
 
-#App settings 
+# App settings
 app = Flask(__name__)
-#Scss(app)
-app.config['SECRET_KEY'] = 'maxseCretPliz18882'
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+# Scss(app)
+app.config["SECRET_KEY"] = "maxseCretPliz18882"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-#Database
-#Zvati svaki put prije pristupa bazi?
+# Database
+# Zvati svaki put prije pristupa bazi?
 db = Memgraph()
 
-#Twitter connector
-twitter_bp = make_twitter_blueprint(api_key='AckLZqFrceE6E2EctXUF1Bmly' , api_secret='CJlkvc0xfQYi731uICahGIJ9mrSw7zYYu7S3EmB7CCftkDASSr')
-app.register_blueprint(twitter_bp, url_prefix='/twitter_login')
-#Twitter connection
-@app.route('/twitter')
+# Twitter connector
+twitter_bp = make_twitter_blueprint(
+    api_key="AckLZqFrceE6E2EctXUF1Bmly",
+    api_secret="CJlkvc0xfQYi731uICahGIJ9mrSw7zYYu7S3EmB7CCftkDASSr",
+)
+app.register_blueprint(twitter_bp, url_prefix="/twitter_login")
+# Twitter connection
+@app.route("/twitter")
 def twitter_login():
-	if not twitter.authorized:
-		return redirect(url_for('twitter.login'))
-	return redirect(url_for('account'))
-#FB connector
-app.config['FACEBOOK_OAUTH_CLIENT_ID'] = ''
-app.config['FACEBOOK_OAUTH_CLIENT_SECRET'] = ''
+    if not twitter.authorized:
+        return redirect(url_for("twitter.login"))
+    return redirect(url_for("account"))
+
+
+# FB connector
+app.config["FACEBOOK_OAUTH_CLIENT_ID"] = ""
+app.config["FACEBOOK_OAUTH_CLIENT_SECRET"] = ""
 facebook_bp = make_facebook_blueprint(rerequest_declined_permissions=True)
 facebook_bp.rerequest_declined_permissions = True
 app.register_blueprint(facebook_bp, url_prefix="/login")
-#FB connection
+# FB connection
 def fb_login():
-	if not facebook.authorized:
-		return redirect(url_for("facebook.login"))
-	return redirect(url_for('account'))
+    if not facebook.authorized:
+        return redirect(url_for("facebook.login"))
+    return redirect(url_for("account"))
 
 
-#App
+# App
 @app.before_request
 def handleSession():
-	if "userEmail" in session:
-		authed = True
-	else:
-		authed = False
-	if twitter.authorized:
-		resp = twitter.get("account/verify_credentials.json", params={"include_email" : "true"})
-		if resp.ok:
-			authed = True
-			resp_json = resp.json()
-			userName = resp_json["screen_name"]
-			userEmail = resp_json["email"]
-	elif facebook.authorized:
-		resp = facebook.get("/me?fields=name,email")
-		if resp.ok and reps.text:
-			authed = True
-			resp_json = resp.json()
-			userName = resp_json["name"]
-			userEmail = resp_json["email"]
-	if authed:
-		res = dbComms.userCheck(db,userEmail)
-		if  res==False:
-			dbComms.userCreate(db,userName,userEmail)
-		if "userName" not in session or "userEmail" not in session:
-			session["userName"] = dbComms.userGetByEmail(db,userEmail).username
-			session["userEmail"] = userEmail		
-	if("warning" in session):
-		session["warningMsg"]=session["warning"]
-		session["warning"]=warnings.noWarning()
-	else:
-		session["warningMsg"]=warnings.noWarning()
-		session["warning"]=warnings.noWarning()
+    if "userEmail" in session:
+        authed = True
+    else:
+        authed = False
+    if twitter.authorized:
+        resp = twitter.get(
+            "account/verify_credentials.json", params={"include_email": "true"}
+        )
+        if resp.ok:
+            authed = True
+            resp_json = resp.json()
+            userName = resp_json["screen_name"]
+            userEmail = resp_json["email"]
+    elif facebook.authorized:
+        resp = facebook.get("/me?fields=name,email")
+        if resp.ok and reps.text:
+            authed = True
+            resp_json = resp.json()
+            userName = resp_json["name"]
+            userEmail = resp_json["email"]
+    if authed:
+        res = dbComms.userCheck(db, userEmail)
+        if res == False:
+            dbComms.userCreate(db, userName, userEmail)
+        if "userName" not in session or "userEmail" not in session:
+            session["userName"] = dbComms.userGetByEmail(db, userEmail).username
+            session["userEmail"] = userEmail
+    if "warning" in session:
+        session["warningMsg"] = session["warning"]
+        session["warning"] = warnings.noWarning()
+    else:
+        session["warningMsg"] = warnings.noWarning()
+        session["warning"] = warnings.noWarning()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-	return render_template("index.html", warning=session["warningMsg"])
+    return render_template("index.html", warning=session["warningMsg"])
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
-	session.clear()
-	session["warning"]=warnings.noWarning()
-	return redirect(url_for('index'))
+    session.clear()
+    session["warning"] = warnings.noWarning()
+    return redirect(url_for("index"))
 
-@app.route('/account', methods=["POST","GET"])
+
+@app.route("/account", methods=["POST", "GET"])
 def account():
-	if(twitter.authorized==False):
-		session["warning"]=warnings.noLogin()
-		return redirect(url_for('index'))
-	if request.method == "POST":
-		if(request.form["nameInput"]!=""):
-			#Change name and session info
-			session["userName"] = dbComms.userChangeName(db,session["userEmail"],request.form["nameInput"])
-			return redirect(url_for('account'))
-	return render_template("account.html",user=session["userName"])
+    if twitter.authorized == False:
+        session["warning"] = warnings.noLogin()
+        return redirect(url_for("index"))
+    if request.method == "POST":
+        if request.form["nameInput"] != "":
+            # Change name and session info
+            session["userName"] = dbComms.userChangeName(
+                db, session["userEmail"], request.form["nameInput"]
+            )
+            return redirect(url_for("account"))
+    return render_template("account.html", user=session["userName"])
 
-#Movies
-#TMDB
-@app.route('/movies', methods=["POST","GET"])
+
+# Movies
+# TMDB
+@app.route("/movies", methods=["POST", "GET"])
 def moviesList():
-	if(twitter.authorized==False):
-		session["warning"]=warnings.noLogin()
-		return redirect(url_for('index'))
-	#Set filter
-	filtered=""
-	if request.method == "POST":
-		retFilt = dbComms.genreGetAll(db)
-		if("genre" in request.form):
-			filtered=request.form["genre"]
-			ret = dbComms.movieGetByGenre(db,filtered)
-		else:
-			ret = dbComms.movieGetAll(db)
-		return render_template("movieList.html", list=ret, filter=retFilt, filtered=filtered)		
-	else:
-		ret = dbComms.movieGetAll(db)
-		retFilt = dbComms.genreGetAll(db)
-		return render_template("movieList.html", list=ret, filter=retFilt)
+    if twitter.authorized == False:
+        session["warning"] = warnings.noLogin()
+        return redirect(url_for("index"))
+    # Set filter
+    filtered = ""
+    if request.method == "POST":
+        retFilt = dbComms.genreGetAll(db)
+        if "genre" in request.form:
+            filtered = request.form["genre"]
+            ret = dbComms.movieGetByGenre(db, filtered)
+        else:
+            ret = dbComms.movieGetAll(db)
+        return render_template(
+            "movieList.html", list=ret, filter=retFilt, filtered=filtered
+        )
+    else:
+        ret = dbComms.movieGetAll(db)
+        retFilt = dbComms.genreGetAll(db)
+        return render_template("movieList.html", list=ret, filter=retFilt)
 
-@app.route('/movies/add/tmdb/<movieId>')
+
+@app.route("/movies/add/tmdb/<movieId>")
 def tmdbAdd(movieId):
-	movieApiController.apiTmdbAddById(db,movieId)
-	return redirect(url_for('moviesList'))
+    movieApiController.apiTmdbAddById(db, movieId)
+    return redirect(url_for("moviesList"))
 
-@app.route('/movies/add/omdb', defaults={"movieTitle" : "Die Hard"})
-@app.route('/movies/add/omdb/<movieTitle>')
+
+@app.route("/movies/add/omdb", defaults={"movieTitle": "Die Hard"})
+@app.route("/movies/add/omdb/<movieTitle>")
 def omdbAdd(movieTitle):
-	movieApiController.apiOmdbAddByTitle(db,movieTitle)
-	return redirect(url_for('moviesList'))
+    movieApiController.apiOmdbAddByTitle(db, movieTitle)
+    return redirect(url_for("moviesList"))
 
-#Default vraca Die Hard
-@app.route('/movies/rating', defaults={"movieId":"tt0095016"})
-@app.route('/movies/rating/<movieId>')
+
+# Default vraca Die Hard
+@app.route("/movies/rating", defaults={"movieId": "tt0095016"})
+@app.route("/movies/rating/<movieId>")
 def getRating(movieId):
-	return dbComms.movieGetRating(db,movieId)
+    return dbComms.movieGetRating(db, movieId)
 
 
-@app.route('/movies/favorite/<movieId>')
+@app.route("/movies/favorite/<movieId>")
 def setFavoriteMovie(movieId=0):
-	if(twitter.authorized==False):
-		session["warning"]=warnings.noLogin()
-		return redirect(url_for('index'))
-	dbComms.userFavoritesMovie(db,session["userEmail"],movieId)
-	return redirect(url_for('moviesList'))
+    if twitter.authorized == False:
+        session["warning"] = warnings.noLogin()
+        return redirect(url_for("index"))
+    dbComms.userFavoritesMovie(db, session["userEmail"], movieId)
+    return redirect(url_for("moviesList"))
 
-@app.route('/movies/un-favorite/<movieId>')
+
+@app.route("/movies/un-favorite/<movieId>")
 def removeFavoriteMovie(movieId=0):
-	if(twitter.authorized==False):
-		session["warning"]=warnings.noLogin()
-		return redirect(url_for('index'))
-	dbComms.userUnFavoritesMovie(db,session["userEmail"],movieId)
-	return redirect(url_for('favoriteMoviesList'))
+    if twitter.authorized == False:
+        session["warning"] = warnings.noLogin()
+        return redirect(url_for("index"))
+    dbComms.userUnFavoritesMovie(db, session["userEmail"], movieId)
+    return redirect(url_for("favoriteMoviesList"))
 
-@app.route('/movies/favorites')
+
+@app.route("/movies/favorites")
 def favoriteMoviesList():
-	if(twitter.authorized==False):
-		session["warning"]=warnings.noLogin()
-		return redirect(url_for('index'))
-	ret = dbComms.movieGetAllFavorited(db,session["userEmail"])
-	return render_template("movieFavs.html", list=ret)
+    if twitter.authorized == False:
+        session["warning"] = warnings.noLogin()
+        return redirect(url_for("index"))
+    ret = dbComms.movieGetAllFavorited(db, session["userEmail"])
+    return render_template("movieFavs.html", list=ret)
 
-#Test method
-@app.route('/movies/favoritesX')
+
+# Test method
+@app.route("/movies/favoritesX")
 def favoriteMoviesListLimited():
-	if(twitter.authorized==False):
-		session["warning"]=warnings.noLogin()
-		return redirect(url_for('index'))
-	ret = dbComms.movieGetFavoritedLimited(db,session["userEmail"])
-	return render_template("movieFavs.html", list=ret)
+    if twitter.authorized == False:
+        session["warning"] = warnings.noLogin()
+        return redirect(url_for("index"))
+    ret = dbComms.movieGetFavoritedLimited(db, session["userEmail"])
+    return render_template("movieFavs.html", list=ret)
 
-@app.route('/movies/like')
+
+@app.route("/movies/like")
 def testRate():
-	ret = dbComms.movieGetRecentlyRated(db)
-	return render_template("movieFavs.html", list=ret);
+    ret = dbComms.movieGetRecentlyRated(db)
+    return render_template("movieFavs.html", list=ret)
 
-#Test DB controls
-@app.route('/db/prg')
+
+# Test DB controls
+@app.route("/db/prg")
 def purge():
-	purgeDatabase()
-	session.clear()
-	session["warning"]=warnings.noWarning()
-	return redirect("/")
+    purgeDatabase()
+    session.clear()
+    session["warning"] = warnings.noWarning()
+    return redirect("/")
+
 
 def purgeDatabase():
-	qry = 'MATCH (node) DETACH DELETE node'
-	db.execute_query(qry)
-	return
+    qry = "MATCH (node) DETACH DELETE node"
+    db.execute_query(qry)
+    return
 
-#TODO
-@app.route('/db/init')
+
+# TODO
+@app.route("/db/init")
 def initDatabase():
-	dbTestInfo.addOmdbMovies(db)
-	dbTestInfo.addTmdbMovies(db)
-	dbTestInfo.addTestUsers(db)
-	dbTestInfo.addTestLikes(db)
-	dbTestInfo.addRandomVotes(db)
-	return redirect("/")
+    dbTestInfo.addOmdbMovies(db)
+    dbTestInfo.addTmdbMovies(db)
+    dbTestInfo.addTestUsers(db)
+    dbTestInfo.addTestLikes(db)
+    dbTestInfo.addRandomVotes(db)
+    return redirect("/")
 
-#Main
-if __name__ == '__main__':
-	app.run(debug=True)
+
+# Get movie recommendations for current user
+@app.route("/recommendations")
+def recommendations():
+    # check if user is currently logged in
+    if "userEmail" in session:
+        # get database id of the current user
+        current_user_id = dbComms.get_user_id_by_email(db, session["userEmail"])
+        # get user-item matrix to use in collaborative filtering
+        matrix = dbComms.get_user_ratings(db, current_user_id)
+        # store all movies from database in list
+        movies = dbComms.get_all_movies(db)
+        # call get_recommendations method that will return a sorted list of movies and their predictions of likeability for the current user
+        predictions = get_recommendations(matrix)
+        # initialize list of movie predictions
+        movie_prediction_list = []
+        # iterate over all predictions
+        for key in predictions:
+            # take in account only positive predictions
+            if predictions[key] > 0:
+                # create Movie object and set its parameters
+                movie = Movie(movies[key].id, movies[key].properties["name"], [], movies[key].properties["releaseDate"], movies[key].properties["overview"], movies[key].properties["directorName"], movies[key].properties["posterPath"])
+                # add created object to list of movie predictions
+                movie_prediction_list.append(movie)
+        # return moviePredictions template view with items of movie_prediction_list
+        return render_template("moviePredictions.html", list=movie_prediction_list)
+    else:
+        # if user is not logged in redirect to login page
+        return redirect("/login")
+
+
+# Main
+if __name__ == "__main__":
+    app.run(debug=True)
--- social_network/dbTestInfo.py
+++ social_network/dbTestInfo.py
@@ -3,79 +3,102 @@ from social_network import dbComms
 from social_network import movieApiController
 import random
 
-minUserId=1
-maxUserId=30
+minUserId = 1
+maxUserId = 30
+
 
 def addTestUsers(db):
-	dbComms.userCreate(db,"Alice","alice@fakemail.com")
-	dbComms.userCreate(db,"Bob","bob@fakemail.com")
-	dbComms.userCreate(db,"George","george@fakemail.com")
-	dbComms.userCreate(db,"Shrek","shrek@fakemail.com")
-	for i in range (minUserId,maxUserId):
-		dbComms.userCreate(db,"FakeUser"+str(i),"fakeuser{0}@fakemail.com".format(str(i)))
+    dbComms.userCreate(db, "Alice", "alice@fakemail.com")
+    dbComms.userCreate(db, "Bob", "bob@fakemail.com")
+    dbComms.userCreate(db, "George", "george@fakemail.com")
+    dbComms.userCreate(db, "Shrek", "shrek@fakemail.com")
+    for i in range(minUserId, maxUserId):
+        dbComms.userCreate(
+            db, "FakeUser" + str(i), "fakeuser{0}@fakemail.com".format(str(i))
+        )
+
 
 def addTmdbMovies(db):
-	for i in range(550,650):
-		movieApiController.apiTmdbAddById(db,i)
+    for i in range(550, 650):
+        movieApiController.apiTmdbAddById(db, i)
+
 
 def addOmdbMovies(db):
-	movieList = ["Shrek","Spider Man","Mister Bean","Avatar","Forrest Gump","Alice in Wonderland","Chernobyl"]
-	for movie in movieList:
-		movieApiController.apiOmdbAddByTitle(db,movie)
+    movieList = [
+        "Shrek",
+        "Spider Man",
+        "Mister Bean",
+        "Avatar",
+        "Forrest Gump",
+        "Alice in Wonderland",
+        "Chernobyl",
+    ]
+    for movie in movieList:
+        movieApiController.apiOmdbAddByTitle(db, movie)
+
+
 def addTestLikes(db):
-	#Alice likes
-	#Men in black,Jaws 2, Secret beyond the door
-	user = "alice@fakemail.com"
-	likes = ["tt0119654","tt0077766","tt0040766"]
-	dislikes = []
-	favorites = ["tt0087332"] # Ghost busters
-	for movie in likes:
-		dbComms.userRateMovie(db,user,movie,1)
-	for movie in dislikes:
-		dbComms.userRateMovie(db,user,movie,0)
-	for movie in favorites:
-		dbComms.userFavoritesMovie(db,user,movie)
-	#Bob likes
-	#Men in black,Jaws 2,Die Hard, Independance Day
-	user = "bob@fakemail.com"
-	likes = ["tt0119654","tt0077766","tt0095016","tt0116629"]
-	favorites = ["tt0137523"] # Fight club
-	dislikes = []
-	for movie in likes:
-		dbComms.userRateMovie(db,user,movie,1)
-	for movie in dislikes:
-		dbComms.userRateMovie(db,user,movie,0)
-	for movie in favorites:
-		dbComms.userFavoritesMovie(db,user,movie)
-	#George likes
-	#2 Fast 2 Furious,Jaws 2,Die Hard, Chernobyl, Shrek
-	user = "george@fakemail.com"
-	likes = ["tt0322259","tt0077766","tt0095016","tt7366338","tt0126029"]
-	favorites = ["tt0137523"] # Fight club
-	dislikes = []
-	#Shrek likes
-	#Shrek,Shrek, FFVII, Monster Inc
-	user = "shrek@fakemail.com"
-	likes = ["tt0126029","tt0126029","tt0385700","tt0198781"]
-	favorites = ["tt0126029"] # Shrek
-	dislikes = ["tt0077766"] # Jaws 2
-	for movie in likes:
-		dbComms.userRateMovie(db,user,movie,1)
-	for movie in dislikes:
-		dbComms.userRateMovie(db,user,movie,0)
-	for movie in favorites:
-		dbComms.userFavoritesMovie(db,user,movie)
+    # Alice likes
+    # Men in black,Jaws 2, Secret beyond the door
+    user = "alice@fakemail.com"
+    likes = ["tt0119654", "tt0077766", "tt0040766"]
+    dislikes = []
+    favorites = ["tt0087332"]  # Ghost busters
+    for movie in likes:
+        dbComms.userRateMovie(db, user, movie, 1)
+    for movie in dislikes:
+        dbComms.userRateMovie(db, user, movie, 0)
+    for movie in favorites:
+        dbComms.userFavoritesMovie(db, user, movie)
+    # Bob likes
+    # Men in black,Jaws 2,Die Hard, Independance Day
+    user = "bob@fakemail.com"
+    likes = ["tt0119654", "tt0077766", "tt0095016", "tt0116629"]
+    favorites = ["tt0137523"]  # Fight club
+    dislikes = []
+    for movie in likes:
+        dbComms.userRateMovie(db, user, movie, 1)
+    for movie in dislikes:
+        dbComms.userRateMovie(db, user, movie, 0)
+    for movie in favorites:
+        dbComms.userFavoritesMovie(db, user, movie)
+    # George likes
+    # 2 Fast 2 Furious,Jaws 2,Die Hard, Chernobyl, Shrek
+    user = "george@fakemail.com"
+    likes = ["tt0322259", "tt0077766", "tt0095016", "tt7366338", "tt0126029"]
+    favorites = ["tt0137523"]  # Fight club
+    dislikes = []
+    # Shrek likes
+    # Shrek,Shrek, FFVII, Monster Inc
+    user = "shrek@fakemail.com"
+    likes = ["tt0126029", "tt0126029", "tt0385700", "tt0198781"]
+    favorites = ["tt0126029"]  # Shrek
+    dislikes = ["tt0077766"]  # Jaws 2
+    for movie in likes:
+        dbComms.userRateMovie(db, user, movie, 1)
+    for movie in dislikes:
+        dbComms.userRateMovie(db, user, movie, 0)
+    for movie in favorites:
+        dbComms.userFavoritesMovie(db, user, movie)
 
-def addRandomVotes(db,limit=40,page=0):
-	movieList = dbComms.movieGetAll(db,limit=limit,page=page)
-	for movie in movieList:
-		for i in range(minUserId,maxUserId):
-			rand = random.randrange(0,11,1)
-			if (rand >= 10):
-				dbComms.userRateMovie(db,"fakeuser{0}@fakemail.com".format(str(i)),movie.id,1)
-				dbComms.userFavoritesMovie(db,"fakeuser{0}@fakemail.com".format(str(i)),movie.id)
-			elif (rand >= 8):
-				dbComms.userRateMovie(db,"fakeuser{0}@fakemail.com".format(str(i)),movie.id,1)
-			elif (rand == 0):
-				dbComms.userRateMovie(db,"fakeuser{0}@fakemail.com".format(str(i)),movie.id,0)
 
+def addRandomVotes(db, limit=40, page=0):
+    movieList = dbComms.movieGetAll(db, limit=limit, page=page)
+    for movie in movieList:
+        for i in range(minUserId, maxUserId):
+            rand = random.randrange(0, 11, 1)
+            if rand >= 10:
+                dbComms.userRateMovie(
+                    db, "fakeuser{0}@fakemail.com".format(str(i)), movie.id, 1
+                )
+                dbComms.userFavoritesMovie(
+                    db, "fakeuser{0}@fakemail.com".format(str(i)), movie.id
+                )
+            elif rand >= 8:
+                dbComms.userRateMovie(
+                    db, "fakeuser{0}@fakemail.com".format(str(i)), movie.id, 1
+                )
+            elif rand == 0:
+                dbComms.userRateMovie(
+                    db, "fakeuser{0}@fakemail.com".format(str(i)), movie.id, 0
+                )
